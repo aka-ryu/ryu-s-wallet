ed0d4085767afa3078e7035ff6085f46894b15f0
aka-ryu <uhas2003@naver.com>
2023-11-13 06:50:08 +0900

    백엔드 포트 고정

--- rwbackend/src/main.ts
+++ rwbackend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 8080;
+  const PORT = 8080;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
 }
