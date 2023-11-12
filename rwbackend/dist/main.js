1743bbd0a1081eb84e7faf4af8f7319b34c52b88
aka-ryu <uhas2003@naver.com>
2023-11-13 04:26:07 +0900

    pm2 추가

--- rwbackend/dist/main.js
+++ rwbackend/dist/main.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
+    const PORT = process.env.PORT || 8080;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(8080);
+    await app.listen(PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
