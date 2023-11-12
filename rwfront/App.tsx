4259ac490802c4a95acdca75f36d05b15dd9990d
aka-ryu <uhas2003@naver.com>
2023-11-12 10:40:28 +0900

    애니메이션 스플래시 추가

--- rwfront/App.tsx
+++ rwfront/App.tsx
@@ -5,13 +5,17 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import LottieSplashScreen from 'react-native-lottie-splash-screen';
 import Navigator from './src/navigation';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    LottieSplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <SafeAreaProvider>
--- rwfront/android/app/src/main/java/com/rwfront/MainActivity.java
+++ rwfront/android/app/src/main/java/com/rwfront/MainActivity.java
@@ -4,13 +4,15 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    SplashScreen.show(this, R.id.lottie);
+    super.onCreate(savedInstanceState);
   }
 
   /**
